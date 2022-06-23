32589238674e60335fa056ac9529e8e477e909e1
PavaniReddy <94516590+PavaniReddy-codilar@users.noreply.github.com>
2022-06-23 12:36:47 +0530

    Add files via upload

---
+++ Unit1/Plugins/Plugin/AroundBreadcrumbsPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Unit1\Plugins\Plugin;
+
+class AroundBreadcrumbsPlugin
+{
+
+    public function aroundAddCrumb(
+        \Magento\Theme\Block\Html\Breadcrumbs $subject, callable $proceed,
+        $crumbName, $crumbInfo
+    )
+    {
+        $crumbInfo['label'] = $crumbInfo['label'].'(!a)';
+        $proceed($crumbName, $crumbInfo);
+    }
+}
